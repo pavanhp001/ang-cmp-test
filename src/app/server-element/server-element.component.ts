e45287ab209184f5d390563c72ef367376cb448f
pavanhp001 <pavan.hp001@gmail.com>
2022-12-09 22:47:24 +0530

    Added alias to custom property

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element:any | undefined;
+  @Input('srvElement') element:any | undefined;
 
   constructor() { }
 

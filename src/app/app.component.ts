fc84635182314924c6b7b3714f1c308565b5b75a
pavanhp001 <pavan.hp001@gmail.com>
2022-12-09 22:44:19 +0530

    mor components and added property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
 
-interface Elements{
-  type: string,
-  name: string,
-  content: string
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements : Elements[] = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  serverElements: any[] = [{type:"test", name:"server Name", content:"server content"}];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -19,6 +19,10 @@ import { ServerElementComponent } from './server-element/server-element.componen
     AppRoutingModule
   ],
   providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
